daed879acd9edf7fd0cb1457b10a1543d6ff19cc
ShyreneChing <shyrene_ching@dlsu.edu.ph>
2019-06-23 22:23:12 +0800

    Added chart on Burger Slaes

--- chart.js
+++ chart.js
@@ -1,150 +1,131 @@
-
 var chart;
-function loadChart(something) {
-    chart = new Chartist.Pie('.ct-chart', {
-      series: [30, 20, 50],
-      labels: [1, 2, 3]
-    }, {
-      donut: true,
-      showLabel: true
-    });
 
-    chart.on('draw', function(data) {
-      if(data.type === 'slice') {
-        // Get the total path length in order to use for dash array animation
-        var pathLength = data.element._node.getTotalLength();
-
-        // Set a dasharray that matches the path length as prerequisite to animate dashoffset
-        data.element.attr({
-          'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-        });
-
-        // Create animation definition while also assigning an ID to the animation for later sync usage
-        var animationDefinition = {
-          'stroke-dashoffset': {
-            id: 'anim' + data.index,
-            dur: 1000,
-            from: -pathLength + 'px',
-            to:  '0px',
-            easing: Chartist.Svg.Easing.easeOutQuint,
-            // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
-            fill: 'freeze'
-          }
-        };
-
-        // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
-        if(data.index !== 0) {
-          animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-        }
-
-        // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
-        data.element.attr({
-          'stroke-dashoffset': -pathLength + 'px'
-        });
-
-        // We can't use guided mode as the animations need to rely on setting begin manually
-        // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
-        data.element.animate(animationDefinition, false);
-      }
-    });
-
-    // For the sake of the example we update the chart every time it's created with a delay of 8 seconds
-    chart.on('created', function() {
-      if(window.__anim21278907124) {
-        clearTimeout(window.__anim21278907124);
-        window.__anim21278907124 = null;
-      }
-      window.__anim21278907124 = setTimeout(chart.update.bind(chart), 10000);
-    });
-
-}
-
-function loaddoughnut(ctx){
-  chart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+function loaddoughnut(ctx) {
+    chart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Krusty Combo', 'Krusty Deluxe', 'Krabby Pattie'],
+            datasets: [{
+                label: 'Number of Sales per Product',
+                data: [12, 19, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(255, 159, 64, 0.6)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    }
-});
+    });
 }
 
 
-function loadBar(ctx){
-  chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+function loadBar(ctx) {
+    chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Krusty Combo', 'Krusty Deluxe', 'Krabby Pattie'],
+            datasets: [{
+                label: 'Number of Sales per Product',
+                data: [12, 19, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(255, 159, 64, 0.6)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    }
-});
+    });
+}
+
+function loadline(ctx) {
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May'],
+            datasets: [{
+                label: 'Krusty Combo',
+                data: [13, 66, 84, 76, 69],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)'
+                
+                ],
+                borderWidth: 1
+            }, {
+                label: 'Krusty Deluxue',
+                data: [10, 95, 84, 24, 95],
+                backgroundColor: [
+                    'rgba(255, 206, 86, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(255, 206, 86, 1)',
+                ],
+                borderWidth: 1
+            }, {
+                label: 'Krabbie Patty',
+                data: [75, 14, 25, 75, 26],
+                backgroundColor: [
+                    'rgba(153, 102, 255, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 1
+            }
+        ]},
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+
+                }]
+            }
+        }
+    });
 }
 
 
-function chartDest(){
-   if(chart != null){
-    // chart.detach();
-     chart.clear();
-     chart.destroy();
-     chart = null;
-   }
-  
+
+function chartDest() {
+    if (chart != null) {
+        // chart.detach();
+        chart.clear();
+        chart.destroy();
+        chart = null;
+    }
+
 }
