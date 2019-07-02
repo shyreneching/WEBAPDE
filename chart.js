71b537b3101b4bcd145f1a0e890e62894825269f
ShyreneChing <shyrene_ching@dlsu.edu.ph>
2019-07-02 23:16:08 +0800

    Added dummy data for Majority of Charts

--- chart.js
+++ chart.js
@@ -1,7 +1,15 @@
 var chart;
 var tabledata;
 var custData;
-
+var prod;
+var productper;
+var kcmonsales;
+var kpmonsales;
+var kdmonsales;
+var kcweksales;
+var kpweksales;
+var kdweksales;
+var rawlist;
 
 // General Customer Sales               Krusty Combo
 // Leatherblack Turtle      GSlb        Leatherblack Turtle     KClb
@@ -20,109 +28,415 @@ var custData;
 // Giant Clam               KPgc        Giant Clam              KDgc
 // Gray Whale               KPgw        Gray Whale              KDgw
 // Sealion                  KPsl        Sealion                 KDsl
-function getcustData(){
+function getcustData() {
     custData = [{
-        id: "GSlb", 
-        percent : 20, 
-    },{
-        id: "GSsm", 
-        percent : 10, 
-    },{
-        id: "GSsh", 
-        percent : 5, 
-    },{
-        id: "GScr", 
-        percent : 20, 
-    },{
-        id: "GSgc", 
-        percent : 10, 
-    },{
-        id: "GSgw", 
-        percent : 30, 
-    },{
-        id: "GSsl", 
-        percent : 5, 
-    },{
-        id: "KClb", 
-        percent : 10, 
-    },{
-        id: "KCsm", 
-        percent : 20, 
-    },{
-        id: "KCsh", 
-        percent : 20, 
-    },{
-        id: "KCcr", 
-        percent : 30, 
-    },{
-        id: "KCgc", 
-        percent : 10, 
-    },{
-        id: "KCgw", 
-        percent : 5, 
-    },{
-        id: "KCsl", 
-        percent : 5, 
-    },{
-        id: "KPlb", 
-        percent : 20, 
-    },{
-        id: "KPsm", 
-        percent : 10, 
-    },{
-        id: "KPsh", 
-        percent : 5, 
-    },{
-        id: "KPcr", 
-        percent : 20, 
-    },{
-        id: "KPgc", 
-        percent : 10, 
-    },{
-        id: "KPgw", 
-        percent : 30, 
-    },{
-        id: "KPsl", 
-        percent : 5, 
-    },{
-        id: "KDlb", 
-        percent : 5, 
-    },{
-        id: "KDsm", 
-        percent : 10, 
-    },{
-        id: "KDsh", 
-        percent : 20, 
-    },{
-        id: "KDcr", 
-        percent : 10, 
-    },{
-        id: "KDgc", 
-        percent : 30, 
-    },{
-        id: "KDgw", 
-        percent : 20, 
-    },{
-        id: "KDsl", 
-        percent : 5, 
-    }
-    ]
+        id: "GSlb",
+        percent: 20,
+    }, {
+        id: "GSsm",
+        percent: 10,
+    }, {
+        id: "GSsh",
+        percent: 5,
+    }, {
+        id: "GScr",
+        percent: 20,
+    }, {
+        id: "GSgc",
+        percent: 10,
+    }, {
+        id: "GSgw",
+        percent: 30,
+    }, {
+        id: "GSsl",
+        percent: 5,
+    }, {
+        id: "KClb",
+        percent: 10,
+    }, {
+        id: "KCsm",
+        percent: 20,
+    }, {
+        id: "KCsh",
+        percent: 20,
+    }, {
+        id: "KCcr",
+        percent: 30,
+    }, {
+        id: "KCgc",
+        percent: 10,
+    }, {
+        id: "KCgw",
+        percent: 5,
+    }, {
+        id: "KCsl",
+        percent: 5,
+    }, {
+        id: "KPlb",
+        percent: 20,
+    }, {
+        id: "KPsm",
+        percent: 10,
+    }, {
+        id: "KPsh",
+        percent: 5,
+    }, {
+        id: "KPcr",
+        percent: 20,
+    }, {
+        id: "KPgc",
+        percent: 10,
+    }, {
+        id: "KPgw",
+        percent: 30,
+    }, {
+        id: "KPsl",
+        percent: 5,
+    }, {
+        id: "KDlb",
+        percent: 5,
+    }, {
+        id: "KDsm",
+        percent: 10,
+    }, {
+        id: "KDsh",
+        percent: 20,
+    }, {
+        id: "KDcr",
+        percent: 10,
+    }, {
+        id: "KDgc",
+        percent: 30,
+    }, {
+        id: "KDgw",
+        percent: 20,
+    }, {
+        id: "KDsl",
+        percent: 5,
+    }]
 
     return custData;
 
 }
 
+function loadactualList() {
+    rawlist = [{
+        id: "GSlb",
+        percent: 20,
+    }, {
+        id: "GSsm",
+        percent: 10,
+    }, {
+        id: "GSsh",
+        percent: 5,
+    }, {
+        id: "GScr",
+        percent: 20,
+    }, {
+        id: "GSgc",
+        percent: 10,
+    }, {
+        id: "GSgw",
+        percent: 30,
+    }, {
+        id: "GSsl",
+        percent: 5,
+    }, {
+        id: "KClb",
+        percent: 10,
+    }, {
+        id: "KCsm",
+        percent: 20,
+    }, {
+        id: "KCsh",
+        percent: 20,
+    }, {
+        id: "KCcr",
+        percent: 30,
+    }, {
+        id: "KCgc",
+        percent: 10,
+    }, {
+        id: "KCgw",
+        percent: 5,
+    }, {
+        id: "KCsl",
+        percent: 5,
+    }, {
+        id: "KPlb",
+        percent: 20,
+    }, {
+        id: "KPsm",
+        percent: 10,
+    }, {
+        id: "KPsh",
+        percent: 5,
+    }, {
+        id: "KPcr",
+        percent: 20,
+    }, {
+        id: "KPgc",
+        percent: 10,
+    }, {
+        id: "KPgw",
+        percent: 30,
+    }, {
+        id: "KPsl",
+        percent: 5,
+    }, {
+        id: "KDlb",
+        percent: 5,
+    }, {
+        id: "KDsm",
+        percent: 10,
+    }, {
+        id: "KDsh",
+        percent: 20,
+    }, {
+        id: "KDcr",
+        percent: 10,
+    }, {
+        id: "KDgc",
+        percent: 30,
+    }, {
+        id: "KDgw",
+        percent: 20,
+    }, {
+        id: "KDsl",
+        percent: 5,
+    }]
 
+    return custData;
 
+}
+
+function loadproducts() {
+    prod = ["Krusty Combo", "Krabbie Pattie", "Krusty Deluxe"]
+}
+
+function loadproductpercent() {
+    productper = [{
+        id: "Krusty Combo",
+        percent: 20
+    }, {
+        id: "Krabbie Pattie",
+        percent: 30
+    }, {
+        id: "Krusty Deluxe",
+        percent: 50
+    }]
+}
+
+// function that load the monthly sales of Krusty Combo
+function loadKCmonthlysales() {
+    kcmonsales = [{
+        id: "January",
+        val: 95
+    }, {
+        id: "February",
+        val: 21
+    }, {
+        id: "March",
+        val: 45
+    }, {
+        id: "April",
+        val: 72
+    }, {
+        id: "May",
+        val: 25
+    }, {
+        id: "June",
+        val: 45
+    }, {
+        id: "July",
+        val: 28
+    }, {
+        id: "August",
+        val: 78
+    }, {
+        id: "September",
+        val: 10
+    }, {
+        id: "October",
+        val: 5
+    }, {
+        id: "November",
+        val: 17
+    }, {
+        id: "December",
+        val: 22
+    }]
+}
+
+// function that load the monthly sales of Krabbie Pattie
+function loadKPmonthlysales() {
+    kpmonsales = [{
+        id: "January",
+        val: 25
+    }, {
+        id: "February",
+        val: 24
+    }, {
+        id: "March",
+        val: 4
+    }, {
+        id: "April",
+        val: 22
+    }, {
+        id: "May",
+        val: 28
+    }, {
+        id: "June",
+        val: 21
+    }, {
+        id: "July",
+        val: 32
+    }, {
+        id: "August",
+        val: 58
+    }, {
+        id: "September",
+        val: 17
+    }, {
+        id: "October",
+        val: 47
+    }, {
+        id: "November",
+        val: 58
+    }, {
+        id: "December",
+        val: 37
+    }]
+}
+
+// function that load the monthly sales of Krusty Deluxe
+function loadKDmonthlysales() {
+    kdmonsales = [{
+        id: "January",
+        val: 17
+    }, {
+        id: "February",
+        val: 25
+    }, {
+        id: "March",
+        val: 45
+    }, {
+        id: "April",
+        val: 82
+    }, {
+        id: "May",
+        val: 35
+    }, {
+        id: "June",
+        val: 56
+    }, {
+        id: "July",
+        val: 68
+    }, {
+        id: "August",
+        val: 2
+    }, {
+        id: "September",
+        val: 38
+    }, {
+        id: "October",
+        val: 78
+    }, {
+        id: "November",
+        val: 52
+    }, {
+        id: "December",
+        val: 45
+    }]
+}
+
+// function that load the weekly sales of Krusty Combo
+function loadKCweeklysales(){
+    kcweksales = [{
+        id: "Sunday",
+        val: 17
+    }, {
+        id: "Monday",
+        val: 25
+    }, {
+        id: "Tuesday",
+        val: 45
+    }, {
+        id: "Wednesday",
+        val: 82
+    }, {
+        id: "Thursday",
+        val: 35
+    }, {
+        id: "Friday",
+        val: 56
+    }, {
+        id: "Saturday",
+        val: 56
+    }]
+}
+
+// function that load the weekly sales of Krabbie Patty
+function loadKPweeklysales(){
+    kpweksales = [{
+        id: "Sunday",
+        val: 24
+    }, {
+        id: "Monday",
+        val: 75
+    }, {
+        id: "Tuesday",
+        val: 58
+    }, {
+        id: "Wednesday",
+        val: 10
+    }, {
+        id: "Thursday",
+        val: 35
+    }, {
+        id: "Friday",
+        val: 85
+    }, {
+        id: "Saturday",
+        val: 21
+    }]
+}
+
+// function that load the weekly sales of Krusty Deluxe
+function loadKDweeklysales(){
+    kdweksales = [{
+        id: "Sunday",
+        val: 47
+    }, {
+        id: "Monday",
+        val: 58
+    }, {
+        id: "Tuesday",
+        val: 69
+    }, {
+        id: "Wednesday",
+        val: 12
+    }, {
+        id: "Thursday",
+        val: 78
+    }, {
+        id: "Friday",
+        val: 28
+    }, {
+        id: "Saturday",
+        val: 72
+    }]
+}
 
 function loaddoughnut(ctx) {
+    
     chart = new Chart(ctx, {
         type: 'doughnut',
         data: {
-            labels: ['Krusty Combo', 'Krusty Deluxe', 'Krabby Pattie'],
+            labels: [productper[0].id, productper[1].id, productper[2].id],
             datasets: [{
                 label: 'Number of Sales per Product',
-                data: [12, 19, 3],
+                data: [productper[0].percent, productper[1].percent, productper[2].percent],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.6)',
                     'rgba(255, 206, 86, 0.6)',
@@ -160,10 +474,10 @@ function loadBar(ctx) {
     chart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Krusty Combo', 'Krusty Deluxe', 'Krabby Pattie'],
+            labels: [productper[0].id, productper[1].id, productper[2].id],
             datasets: [{
                 label: 'Number of Sales per Product',
-                data: [12, 19, 3],
+                data: [productper[0].percent, productper[1].percent, productper[2].percent],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.6)',
                     'rgba(255, 206, 86, 0.6)',
@@ -190,13 +504,22 @@ function loadBar(ctx) {
 }
 
 function loadline(ctx) {
+    var kctemp = []
+    var kptemp = []
+    var kdtemp = []
+    for(var i = 0; i < kcmonsales.length; i++){
+        kctemp.push(kcmonsales[i].val)
+        kptemp.push(kpmonsales[i].val)
+        kdtemp.push(kdmonsales[i].val)
+    }
+
     chart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
             datasets: [{
                 label: 'Krusty Combo',
-                data: [13, 66, 84, 76, 69, 13, 66, 13, 66, 84, 76, 69],
+                data: kctemp,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.5)'
                 ],
@@ -206,23 +529,23 @@ function loadline(ctx) {
                 ],
                 borderWidth: 1
             }, {
-                label: 'Krusty Deluxue',
-                data: [10, 95, 84, 24, 95],
-                backgroundColor: [
-                    'rgba(255, 206, 86, 0.5)'
-                ],
-                borderColor: [
-                    'rgba(255, 206, 86, 1)',
-                ],
-                borderWidth: 1
-            }, {
                 label: 'Krabbie Patty',
-                data: [75, 14, 25, 75, 26],
+                data: kptemp,
                 backgroundColor: [
                     'rgba(153, 102, 255, 0.5)'
                 ],
                 borderColor: [
                     'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: 'Krusty Deluxue',
+                data: kdtemp,
+                backgroundColor: [
+                    'rgba(255, 206, 86, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(255, 206, 86, 1)',
                 ],
                 borderWidth: 1
             }]
@@ -241,13 +564,22 @@ function loadline(ctx) {
 }
 
 function loadweek(ctx) {
+    var kctemp = []
+    var kptemp = []
+    var kdtemp = []
+    for(var i = 0; i < kcweksales.length; i++){
+        kctemp.push(kcweksales[i].val)
+        kptemp.push(kpweksales[i].val)
+        kdtemp.push(kdweksales[i].val)
+    }
+
     chart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
             datasets: [{
                 label: 'Krusty Combo',
-                data: [13, 66, 84, 76, 69, 13, 66],
+                data: kctemp,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.5)'
                 ],
@@ -257,23 +589,23 @@ function loadweek(ctx) {
                 ],
                 borderWidth: 1
             }, {
-                label: 'Krusty Deluxue',
-                data: [10, 95, 84, 24, 95, 45, 26, 45],
-                backgroundColor: [
-                    'rgba(255, 206, 86, 0.5)'
-                ],
-                borderColor: [
-                    'rgba(255, 206, 86, 1)',
-                ],
-                borderWidth: 1
-            }, {
                 label: 'Krabbie Patty',
-                data: [75, 14, 25, 75, 26, 0, 0, 0, 0],
+                data: kptemp,
                 backgroundColor: [
                     'rgba(153, 102, 255, 0.5)'
                 ],
                 borderColor: [
                     'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: 'Krusty Deluxue',
+                data: kdtemp,
+                backgroundColor: [
+                    'rgba(255, 206, 86, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(255, 206, 86, 1)',
                 ],
                 borderWidth: 1
             }]
@@ -293,13 +625,14 @@ function loadweek(ctx) {
 
 
 function loadSummary(ctx) {
+
     chart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Krusty Combo', 'Krusty Deluxue', 'Krabbie Patty'],
+            labels: ['Krusty Combo', 'Krabbie Patty', 'Krusty Deluxue'],
             datasets: [{
                 label: 'Leatherblack Turtle',
-                data: [13, 66, 84],
+                data: [rawlist[rawlist.findIndex(x => x.id == "KClb")].percent,rawlist[rawlist.findIndex(x => x.id == "KPlb")].percent,rawlist[rawlist.findIndex(x => x.id == "KDlb")].percent],
                 backgroundColor: [
                     'rgba(33, 33, 33, 0.5)',
                     'rgba(33, 33, 33, 0.5)',
@@ -314,7 +647,7 @@ function loadSummary(ctx) {
                 borderWidth: 1
             }, {
                 label: 'Salmon',
-                data: [10, 95, 84],
+                data: [rawlist[rawlist.findIndex(x => x.id == "KCsm")].percent,rawlist[rawlist.findIndex(x => x.id == "KPsm")].percent,rawlist[rawlist.findIndex(x => x.id == "KDsm")].percent],
                 backgroundColor: [
                     'rgba(250, 128, 114, 0.5)',
                     'rgba(250, 128, 114, 0.5)',
@@ -328,7 +661,7 @@ function loadSummary(ctx) {
                 borderWidth: 1
             }, {
                 label: 'Seahorse',
-                data: [75, 14, 25],
+                data: [rawlist[rawlist.findIndex(x => x.id == "KCsh")].percent,rawlist[rawlist.findIndex(x => x.id == "KPsh")].percent,rawlist[rawlist.findIndex(x => x.id == "KDsh")].percent],
                 backgroundColor: [
                     'rgba(240, 173, 78, 0.5)',
                     'rgba(240, 173, 78, 0.5)',
@@ -342,7 +675,7 @@ function loadSummary(ctx) {
                 borderWidth: 1
             }, {
                 label: 'Coral',
-                data: [75, 84, 35],
+                data: [rawlist[rawlist.findIndex(x => x.id == "KCcr")].percent,rawlist[rawlist.findIndex(x => x.id == "KPcr")].percent,rawlist[rawlist.findIndex(x => x.id == "KDcr")].percent],
                 backgroundColor: [
                     'rgba(255, 127, 80, 0.5)',
                     'rgba(255, 127, 80, 0.5)',
@@ -356,7 +689,7 @@ function loadSummary(ctx) {
                 borderWidth: 1
             }, {
                 label: 'Giant Clam',
-                data: [65, 10, 71],
+                data: [rawlist[rawlist.findIndex(x => x.id == "KCgc")].percent,rawlist[rawlist.findIndex(x => x.id == "KPgc")].percent,rawlist[rawlist.findIndex(x => x.id == "KDgc")].percent],
                 backgroundColor: [
                     'rgba(64, 168, 204, 0.5)',
                     'rgba(64, 168, 204, 0.5)',
@@ -370,7 +703,7 @@ function loadSummary(ctx) {
                 borderWidth: 1
             }, {
                 label: 'Gray Whale',
-                data: [15, 44, 75],
+                data: [rawlist[rawlist.findIndex(x => x.id == "KCgw")].percent,rawlist[rawlist.findIndex(x => x.id == "KPgw")].percent,rawlist[rawlist.findIndex(x => x.id == "KDgw")].percent],
                 backgroundColor: [
                     'rgba(128, 128, 128, 0.5)',
                     'rgba(128, 128, 128, 0.5)',
@@ -384,7 +717,7 @@ function loadSummary(ctx) {
                 borderWidth: 1
             }, {
                 label: 'Sea Lion',
-                data: [65, 49, 95],
+                data: [rawlist[rawlist.findIndex(x => x.id == "KCsl")].percent,rawlist[rawlist.findIndex(x => x.id == "KPsl")].percent,rawlist[rawlist.findIndex(x => x.id == "KDsl")].percent],
                 backgroundColor: [
                     'rgba(255, 165, 0, 0.5)',
                     'rgba(255, 165, 0, 0.5)',
@@ -396,8 +729,7 @@ function loadSummary(ctx) {
                     'rgba(255, 165, 0, 1)'
                 ],
                 borderWidth: 1
-            }
-        ]
+            }]
         },
         options: {
             scales: {
@@ -459,37 +791,37 @@ function CreateTableFromJSON(data) {
     //     }
     // }
 
-    var col = ['No.','Datetime','Burger','Species'];
-    
+    var col = ['No.', 'Datetime', 'Burger', 'Species'];
+
     var table = document.createElement("table");
-    var tr = table.insertRow(-1);                   // TABLE ROW.
+    var tr = table.insertRow(-1); // TABLE ROW.
 
     for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");      // TABLE HEADER.
+        var th = document.createElement("th"); // TABLE HEADER.
         th.innerHTML = col[i];
         tr.appendChild(th);
     }
 
     var ctr = 0;
-    for(var m = 0; m < tabledata.length; m++) {
+    for (var m = 0; m < tabledata.length; m++) {
         tr = table.insertRow(-1);
         var data = tabledata[m];
-      
-        for(var i = 0; i < col.length; i++) {
-            var tabCell = tr.insertCell(-1);            
-            if(i == 0) {
-                tabCell.innerHTML = ctr+1;
-            } else if(i == 1) {
+
+        for (var i = 0; i < col.length; i++) {
+            var tabCell = tr.insertCell(-1);
+            if (i == 0) {
+                tabCell.innerHTML = ctr + 1;
+            } else if (i == 1) {
                 tabCell.innerHTML = data.datetime;
-            } else if(i == 2) {
+            } else if (i == 2) {
                 tabCell.innerHTML = data.burger;
-            } else if(i == 3) {
+            } else if (i == 3) {
                 tabCell.innerHTML = data.species;
             }
         }
         ctr++;
     }
-    
+
     return table;
     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
     // var divContainer = document.getElementById("table");
