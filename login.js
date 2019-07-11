57e0503e4816508417707c19742d803f671e112c
MylesRC <myles_chan@dlsu.edu.ph>
2019-07-11 10:36:13 +0800

    something

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#warning").hide();
 
     $(".login_btn").click(function () { 
-        if($(".username_input").val() != "admin" || $(".password_input").val() != "password")
+        if($(".username_input").val() != "admin" || $(".password_input").val() != "p@ssword")
             $("#warning").show();
         else
             window.location.href= 'AddFile.html';
